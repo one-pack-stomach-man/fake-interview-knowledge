58423f8b4c23d1bebffbe4b54dc1a8d4de22b3c3
chenyaozong <18652844803@163.com>
2021-06-10 10:45:48 +0800

    Update

---
+++ block.js
@@ -0,0 +1,25 @@
+/**
+ * display block 和 inline的区别
+ * 
+ * block元素的特点: 
+ *  block元素占据一行 
+ *  如果没有设置宽度 会自动填充父容器 
+ *  可以应用margin padding 
+ *  
+ * inline:
+ * 前后不会强制换行
+ * line-height 有效果
+ * padding margin 在水平方向上有效 垂直方向上无效
+ * 自动换行
+ * 
+ * 
+ * 
+ * inline-block:
+ *  之后不会换行
+ *  margin-top bottom padding-bottom top 生效
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
